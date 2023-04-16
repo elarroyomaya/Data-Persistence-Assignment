8bc2af6980afff5af03167a4512bb5e8c37abc0a
elarroyomaya <nicolas.delieutraz@yahoo.com>
2023-04-16 17:54:24 +0200

    Player Name query implementation
    - Implementation of a Menu scene
    
    - Implementation of a singleton Main Manager that persist between scenes
    
    - Add of a player name input field and a start button
    
    - Player name persist into main scene, and is displayer side to the current score

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
+
+// Sets the script to be executed later than all default scripts
+// This is helpful for UI, since other things may need to be initialized before setting the UI
+[DefaultExecutionOrder(1000)]
+public class MenuUIHandler : MonoBehaviour
+{
+
+    public InputField enterPlayerName;
+
+    private void Start()
+    {
+
+    }
+
+    public void StartNewGame() 
+    {
+        MainManager.Instance.SetName(enterPlayerName.text);
+        SceneManager.LoadScene(1);
+    }
+
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+#else
+            Application.Quit();
+#endif
+
+    }
+    public void SaveName()
+    {
+        
+    }
+
+    public void LoadName()
+    {
+       
+    }
+}
